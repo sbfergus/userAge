22c79695715e4ab55f24b82f3ab3410571c73325
Scott <sbferguson17@gmail.com>
2022-09-21 11:55:36 -0600

    add styles to form inputs

--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -1,4 +1,5 @@
 import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
 
 const AddUser = () => {
   const addUserHandler = (e) => {
@@ -6,7 +7,7 @@ const AddUser = () => {
   };
 
   return (
-    <Card>
+    <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
         <input id="username" type="text" />
